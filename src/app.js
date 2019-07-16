6e547becba0304de9985ac025dd5d29c8bcd444a
ollie <soulshaymin16@gmail.com>
2019-07-15 17:59:23 -0700

    began working on javascript

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const quizButton = document.getElementById('quiz-button');
+const quizResult = document.getElementById('quiz-result');
+
+quizButton.onClick = function() {
+
+};
+
+const isYes = function() {
+
+};
