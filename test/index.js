0332c1aac0077b7860c3eb44e48bd84e37cad3d7
ollie <soulshaymin16@gmail.com>
2019-07-17 12:10:49 -0700

    fixed index.js

--- test/index.js
+++ test/index.js
@@ -14,4 +14,4 @@ global.URLSearchParams = window.URLSearchParams;
 global.URL = window.URL;
 
 require = require('esm')(module);
-module.exports = require('./index.js');
+module.exports = require('./tests.js');
