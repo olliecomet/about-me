3f5da1c99fe8910bc6cf1ed99341fb9a61f2ee8f
ollie <soulshaymin16@gmail.com>
2019-07-17 11:22:42 -0700

    added app functionality to guess

--- src/guess.js
+++ src/guess.js
@@ -1,11 +1,57 @@
-// what needs to be imported from other js files?
+// needed import from js files?
+import compareNumbers from './compare-numbers.js';
 
-// what is needed from the DOM elements 
+// needed DOM elements 
+const guessButton = document.getElementById('guess-button');
+const userGuess = document.getElementById('number');
+const tooHighLow = document.getElementById('too-high-low');
+const winOrLoss = document.getElementById('win-or-loss');
+const winImage = document.getElementById('win-image');
+const lossImage = document.getElementById('loss-image');
 
-// what state needs to be tracked
+// initialized states
+const correctNumber = 8;
+let tries = 4;
 
-// initialize that state 
+guessButton.addEventListener('click', () => {
+    // storing the user's guess 
+    const guess = parseInt(userGuess.value);
 
-// what event needs to be listened to?
+    const result = compareNumbers(guess, correctNumber);
 
-    // add events below
+    // conditions to determine if user wins or loses
+    if(result === 0) {
+        
+        winOrLoss.textContent = ('You got it!');
+
+        winImage.classList.remove('hidden');
+
+        guessButton.disabled = true;
+    }
+    else if(result === -1) {
+        tooHighLow.textContent = ('Your guess was too low!')
+
+        tries -= 1;
+
+        if(tries === 0) {
+            winOrLoss.textContent = ('You lost!');
+
+            lossImage.classList.remove('hidden');
+
+            guessButton.disabled = true;
+        }
+    }
+    else if(result === 1) {
+        tooHighLow.textContent = ('Your guess was too high!');
+
+        tries -= 1;
+
+        if(tries === 0){
+            winOrLoss.textContent = ('You lost!');
+
+            lossImage.classList.remove('hidden');
+    
+            guessButton.disabled = true;
+        }
+    }  
+});    
