7e6a267a559d046567ec51977de9917d165d6fed
ollie <soulshaymin16@gmail.com>
2019-07-18 09:14:26 -0700

    completed tdd on get throw from number function

--- src/get-throw.js
+++ src/get-throw.js
@@ -2,6 +2,14 @@ export default function getThrow() {
 
 }
 
-export function getThrowFromNumber() {
-
+export function getThrowFromNumber(number) {
+    if(number === 0) {
+        return 'rock';
+    }
+    else if(number === 1) {
+        return 'paper';
+    }
+    else if(number === 2) {
+        return 'scissors';
+    }
 }
